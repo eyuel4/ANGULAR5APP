1b9fa2ff16ce09cbc482699c6a40c7bf2eb92da1
taddesee <eyuel_taddese@wgresorts.com>
2017-11-29 11:47:44 -0500

    linked components together Section 5 completed

--- src/app/recipes/recipeList/recipeList.component.ts
+++ src/app/recipes/recipeList/recipeList.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipes.model';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
     selector:'app-recipeList',
     templateUrl:'../recipeList/recipeList.component.html',
     styleUrls:['../recipeList/recipeList.component.css']
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
+    recipes : Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test',"https://get.pxhere.com/photo/dish-food-salad-vegetable-recipe-meat-beef-steak-kale-garnish-dining-refreshments-tenderloin-venison-piement-beef-tenderloin-horseradish-rib-eye-steak-roast-beef-sirloin-steak-meat-chop-flat-iron-steak-tafelspitz-1376527.jpg")
+    ];
+
     constructor() {
 
+    }
+
+    ngOnInit(): void {
+    
     }
 }
--- src/app/shoppingList/shoppingList.component.ts
+++ src/app/shoppingList/shoppingList.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core'
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { Ingredient } from '../shared/model/ingredient.model';
 
 @Component({
     selector:'app-shoppingList',
@@ -6,8 +8,16 @@ import { Component } from '@angular/core'
     styleUrls:['../shoppingList/shoppingList.component.css']
 })
 
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit {
+    ingredients: Ingredient[]= [
+        new Ingredient('Apples', 5),
+        new Ingredient('Tomatoes', 15)
+    ];
+
     constructor() {
 
+    }
+
+    ngOnInit(): void {
     }
 }
