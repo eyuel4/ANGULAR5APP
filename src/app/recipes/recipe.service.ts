b97859801488ca268dc4c2cf76e6e7ffbcd9f6c2
taddesee <eyuel_taddese@wgresorts.com>
2017-12-29 16:50:07 -0500

    Adding Ingredient Controls to a FormArray

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,16 @@
 import { Recipe } from './recipes.model';
 import {Injectable } from '@angular/core';
+import { Subject } from 'rxjs/Subject';
 
 import { Ingredient } from '../shared/model/ingredient.model';
 
 import { ShoppingListService } from '../shoppingList/shoppingList.service';
 
+
 @Injectable()
 export class RecipeService {
+    recipesChanged = new Subject<Recipe[]>();
+
     private recipes : Recipe[] = [
         new Recipe(
             'A Test Recipe', 
@@ -40,5 +44,15 @@ export class RecipeService {
 
     addIngredientsToShoppingList(ingredients: Ingredient[]) {
         this.shoppingListService;
+    }
+
+    addRecipe(recipe: Recipe) {
+        this.recipes.push(recipe);
+        this.recipesChanged.next(this.recipes.slice());
+    }
+
+    updateRecipe(index: number, newRecipe: Recipe) {
+        this.recipes[index] = newRecipe;
+        this.recipesChanged.next(this.recipes.slice());
     }
 }
