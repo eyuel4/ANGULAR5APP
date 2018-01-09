4dd6790594033210e2ac158728e52413f05a99ee
taddesee <eyuel_taddese@wgresorts.com>
2018-01-09 16:58:17 -0500

    Remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,21 +5,10 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipeList/recipeList.component';
-import { RecipeItemComponent } from './recipes/recipeList/recipeItem/recipeItem.component';
-import { ShoppingListComponent } from './shoppingList/shoppingList.component';
-import { ShoppingListEditComponent } from './shoppingList/shoppingListEdit/shoppingListEdit.component'
-import { RecipeDetailComponent } from './recipes/recipeDetail/recipeDetail.component';
-import { DropDownDirective } from './shared/directive/dropdown.directive';
 import { ShoppingListService } from './shoppingList/shoppingList.service';
 import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/service/data-storage.service';
-import { SigninComponent } from './auth/signin/signin.component';
-import { SignupComponent } from './auth/signup/signup.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
 import { RecipesModule } from './recipes/recipes.module';
